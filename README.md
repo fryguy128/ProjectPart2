56c3e287d35905f195f62f3690c1cd4bf98e5a26
Patrick Duensing <fryguy@fryguy128.com>
2017-05-03 21:25:28 -0500

    Added histogram script

---
+++ README.md
@@ -0,0 +1,2 @@
+# Steady Hand Part 2
+This project contains the scripts necessary to to complete the objectives of the steady hand project part 2. Each script from part 1 has been adjusted to act upon the data that was recorded for the first second and first ten seconds.
---
+++ scripts/averageDist.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+
+import fileinput
+import math
+
+x = 0
+sum = 0
+numbers = []
+for i in fileinput.input():
+    numbers.append(float(i.strip()))
+    sum += numbers[x]
+    x+= 1
+avg = sum/x
+sum = 0
+for i in numbers:
+    print(math.fabs(i-avg))
